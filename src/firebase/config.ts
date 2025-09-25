114e1dc1253e14fa0dfcf0551b1e52d149971836
Jagadeesh <me.jagadeesh.225@gmail.com>
2025-09-25 22:05:18 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREbase_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  "projectId": "studio-1903100306-99225",
+  "appId": "1:1065957868013:web:46988ae9d5fa1d21e5e7e4",
+  "apiKey": "AIzaSyC6bz1W8ipS5o57nOs9Wu97K2337ZF8PQs",
+  "authDomain": "studio-1903100306-99225.firebaseapp.com",
+  "measurementId": "G-TPFJQ5ZXV3",
+  "messagingSenderId": "1065957868013"
 };
