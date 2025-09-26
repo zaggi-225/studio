1aaf17c31a0b8ffc0dee59d529847618e5e1e8a4
Jagadeesh <me.jagadeesh.225@gmail.com>
2025-09-26 08:04:33 +0000

    Goal: Create a native Android app version of our web app https://studio-

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -10,6 +11,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
